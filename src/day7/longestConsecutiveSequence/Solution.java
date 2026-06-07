1334f44d43b037ae97afafaa9ff12113aa99d799
adiya <adiyatakhellambam@gmail.com>
2026-06-07 21:44:16 +0530

    Completion of Day 7 Challenge

---
+++ src/day7/longestConsecutiveSequence/Solution.java
@@ -0,0 +1,36 @@
+package day7.longestConsecutiveSequence;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+    //optimal solution using hashset
+    public int longestConsecutive(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        int longestStreak = 1;
+
+        for (int num : set) {
+            if (!set.contains(num - 1)) {
+                int currentNum = num;
+                int currentStreak = 1;
+
+                while (set.contains(currentNum + 1)) {
+                    currentNum++;
+                    currentStreak++;
+                }
+
+                longestStreak = Math.max(longestStreak, currentStreak);
+            }
+        }
+
+        return longestStreak;
+    }
+}
